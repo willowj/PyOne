d7d3cd13d2a10bbb2e02e894ca54b0bc3e5137c7
abbeyokgo <tumbzzc@qq.com>
2018-12-10 15:09:53 +0800

    update12.10

--- update.sh
+++ update.sh
@@ -8,12 +8,8 @@ function del_rubbish(){
 #执行
 echo "2018.11.20更新版本，修复了磁力链接下载的bug&上传、展示有特殊字符的文件出问题的bug。"
 echo "2018.11.21更新版本，优化磁力下载功能-可选下载文件。"
-echo "2018.14.04更新版本，优化磁力下载界面"
-echo "本次更新会删除离线下载历史数据"
-del_rubbish
-#之前的更新操作
-install_aria2
-update_config
+echo "2018.12.04更新版本，优化磁力下载界面"
+echo "2018.12.10更新版本，修复特定分享目录后，二级目录设置密码出错的bug"
 supervisorctl -c supervisord.conf restart pyone
 echo "---------------------------------------------------------------"
 echo "更新完成！"
